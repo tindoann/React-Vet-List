6b6d0b58bd2924fbb1125a1caee4b68945273e50
tindoann <tindoann@gamil.com>
2020-06-04 00:24:36 -0400

    added app component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
